d5008179c4d1c805b1f8c52ae2efa861852d61c8
alex <alexisriverox@gmail.com>
2015-08-05 16:40:49 -0400

    Added even more stuff

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ var app = express();
 var swig = require('swig');
 
 var routes = require("./routes");
-app.use("/tweets", routes);
+app.use("/", routes, function(req,res) {
+  console.log(req.method + req.path + res.statusCode);
+  res.send(res.contents);
+});
+// , function(request, response) {
+//   console.log(request.method + request.path + response.statusCode);
+// });
 
 // swig setup
 swig.setDefaults({ cache: false });
@@ -12,45 +18,12 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html' );
 app.set('views', __dirname + '/views');
 
-//var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
-
-
-// app.post("/", function(request,response){
-//   console.dir(response);
-//   console.log(request.logx);
+// app.use( function(request, response, next) {
+//   request.logx = request.method + request.path;
+//   console.log(request.method + request.path  + " " +response.statusCode );
+//   response.send(response.contents);
 // });
-
-
-/*
-
-app.get("/tweets/:number", function(request, response, next) {
-  var tweetNumber = request.params.number;
-  console.log(request.params.number);
-  var data = tweetBank.list();
-  response.status(200);
-  response.contents = data[tweetNumber];
-  next();
-
-});
-
-app.get("/people", function(request,response, next){
-  var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-  response.render( 'index', {title: 'Hall of Fame', people: people }, function(err,html){
-    response.contents = html;
-    next();
-  } );
-
-
-});
-
-*/
-
-app.use( function(request, response, next) {
-  request.logx = request.method + request.path;
-  console.log(request.method + request.path  + " " +response.statusCode );
-  response.send(response.contents);
-});
 
 var port = 3000;
 app.listen(port, function () {
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,36 @@
-
+var fs = require('fs');
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
 
-router.get('/dev', function (req, res) {
-  var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+router.get('/**', function(req,res,next) {
+  //  console.log("xxx" + req.path);
+    var filePath = path.join(__dirname, '../public', req.path);
+    console.log(filePath);
+  //console.log(__dirname + "/../public" + req.path)
+    fs.exists(filePath, function(exists){
+      if(exists) {
+        res.sendFile(filePath);
+      } else {
+        next();
+      }
+    });
+  //res.sendFile(path.join(__dirname, '../public/stylesheets', 'style.css'));
+  //next();
+
 });
+
+router.get('/tweets', function (req, res, next) {
+  console.log("tweets called")
+  var tweets = tweetBank.list();
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets }, function(err, html){
+    //res.status(200);
+    res.contents = html;
+    next();
+  });
+});
+
 
 module.exports = router;
